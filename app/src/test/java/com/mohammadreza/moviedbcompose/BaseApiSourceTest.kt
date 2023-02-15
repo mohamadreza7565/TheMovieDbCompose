37bbda36711b5944c343cb509115ae7b253e5cb5
Mohammadreza <engineer.it93@gmail.com>
2023-02-15 14:40:33 +0330

    Write unit test for BaseApiSource

---
+++ app/src/test/java/com/mohammadreza/moviedbcompose/BaseApiSourceTest.kt
@@ -0,0 +1,64 @@
+package com.mohammadreza.moviedbcompose
+
+
+import com.mohammadreza.moviedbcompose.core.base.BaseApiDataState
+import com.mohammadreza.moviedbcompose.core.base.BaseDataSource
+import com.mohammadreza.moviedbcompose.data.api.MovieApiService
+import io.mockk.mockk
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.concurrent.TimeUnit
+
+/**
+ * Create by Mohammadreza Allahgholi
+ *  Site: https://seniorandroid.ir
+ */
+
+
+class BaseApiSourceTest : BaseDataSource() {
+
+    private val mMovieApiService = mockk<MovieApiService>(relaxed = true)
+
+    @Test
+    @Timeout(value = 60, unit = TimeUnit.SECONDS)
+    fun callApiSuccess() = runTest {
+
+        val isSuccess = callApi {
+            mMovieApiService.getPopularMovies(1)
+        }.last() is BaseApiDataState.Success
+
+        assertEquals(isSuccess,true)
+
+    }
+
+    @Test
+    @Timeout(value = 60, unit = TimeUnit.SECONDS)
+    fun callApiError() = runTest {
+
+        val isError = callApi {
+            mMovieApiService.getPopularMovies(1)
+        }.last() is BaseApiDataState.Error
+
+        assertEquals(isError,true)
+
+    }
+
+    @Test
+    @Timeout(value = 60, unit = TimeUnit.SECONDS)
+    fun callApiLoading() = runTest {
+
+        val isLoading = callApi {
+            mMovieApiService.getPopularMovies(1)
+        }.first() is BaseApiDataState.Loading
+
+        assertEquals(isLoading,true)
+
+    }
+
+
+}
